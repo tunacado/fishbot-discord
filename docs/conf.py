d120b402cd3717ddb7d5ea15f621c5bd8a5b1d11
Robb "tunacado" Manes <contact@tunacado.gg>
2020-11-05 12:45:37 -0500

    Change to default readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_pdj_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -32,6 +31,7 @@ release = '1.0'
 # ones.
 extensions = [
     'recommonmark',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,11 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_pdj_theme'
-htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
-html_theme_options = {
-    'style': 'darker'
-}
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
