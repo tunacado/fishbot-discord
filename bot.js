f994e7258a5d20638c1c9c390b0192a9e46d3e3f
Robb "tunacado" Manes <contact@tunacado.gg>
2020-11-05 11:54:38 -0500

    Add plugin functionality events

--- bot.js
+++ bot.js
@@ -46,12 +46,8 @@ loadPlugins(
         log.error(`Error loading plugins: ${err}`);
     })
 
-// Create event handlers for bot
-bot.on('ready', () => {
-    log.info({ bot }, `Logged in to Discord as ${bot.user.tag}`);
-});
-
 bot.setInterval(() => {
+    // Run periodically for plugins
     plugins.forEach(async function (plugin) {
         if (typeof plugin.periodic === "function") {
             await plugin.periodic();
@@ -60,6 +56,7 @@ bot.setInterval(() => {
 }, config.get('bot.intervalSeconds') * 1000);
 
 bot.on('message', async receivedMessage => {
+    // Any message is received
     plugins.forEach(async function (plugin) {
         if (typeof plugin.onMessage === "function") {
             await plugin.onMessage(receivedMessage);
@@ -67,8 +64,19 @@ bot.on('message', async receivedMessage => {
     });
 });
 
-// Only works on cached messages
+bot.on('messageDelete', async message => {
+    // Any message is received
+    plugins.forEach(async function (plugin) {
+        if (typeof plugin.onMessageDelete === "function") {
+            await plugin.onMessageDelete(message);
+        }
+    });
+});
+
+
 bot.on('messageReactionAdd', async (messageReaction, user) => {
+    // User adds a reaction
+    // Only works on cached messages
     plugins.forEach(async function (plugin) {
         if (typeof plugin.onReactionAdd === "function") {
             await plugin.onReactionAdd(messageReaction, user);
@@ -76,8 +84,9 @@ bot.on('messageReactionAdd', async (messageReaction, user) => {
     });
 });
 
-// Only works on cached messages
 bot.on('messageReactionRemove', async (messageReaction, user) => {
+    // User removes a reaction
+    // Only works on cached messages
     plugins.forEach(async function (plugin) {
         if (typeof plugin.onReactionRemove === "function") {
             await plugin.onReactionRemove(messageReaction, user);
@@ -86,11 +95,86 @@ bot.on('messageReactionRemove', async (messageReaction, user) => {
 });
 
 bot.on('guildMemberAdd', member => {
+    // User joins the server
     plugins.forEach(async function (plugin) {
-        if (typeof plugin.onUserJoin === "function") {
-            await plugin.onUserJoin(member);
+        if (typeof plugin.onUserJoinServer === "function") {
+            await plugin.onUserJoinServer(member);
         }
     });
+});
+
+bot.on('guildMemberRemove', member => {
+    // User leaves the server
+    plugins.forEach(async function (plugin) {
+        if (typeof plugin.onUserLeaveServer === "function") {
+            await plugin.onUserLeaveServer(member);
+        }
+    });
+});
+
+bot.on('guildMemberUpdate', (oldMember, newMember) => {
+    // User is updated
+    plugins.forEach(async function (plugin) {
+        if (typeof plugin.onUserUpdate === "function") {
+            await plugin.onUserUpdate(oldMember, newMember);
+        }
+    });
+});
+
+bot.on('guildUpdate', (oldGuild, newGuild) => {
+    // Server is updated
+    plugins.forEach(async function (plugin) {
+        if (typeof plugin.onServerUpdate === "function") {
+            await plugin.onServerUpdate(oldGuild, newGuild);
+        }
+    });
+});
+
+bot.on('presenceUpdate', (oldPresence, newPresence) => {
+    // Server is updated
+    plugins.forEach(async function (plugin) {
+        if (typeof plugin.onUserStateChange === "function") {
+            await plugin.onUserStateChange(oldPresence, newPresence);
+        }
+    });
+});
+
+
+bot.on('voiceStateUpdate', (oldState, newState) => {
+    if(oldState.voiceChannel === undefined && newState.voiceChannel !== undefined) {
+        // User joins a channel
+        plugins.forEach(async function (plugin) {
+            if (typeof plugin.onUserJoinChannel === "function") {
+                await plugin.onUserJoinChannel(newState.channel);
+            }
+        });
+    } else if(oldState.voiceChannel === undefined) {
+        // User leaves a channel
+        plugins.forEach(async function (plugin) {
+            if (typeof plugin.onUserLeaveChannel === "function") {
+                await plugin.onUserLeaveChannel(oldstate.channel);
+            }
+        });
+    } else if(oldState.mute && !newState.mute) {
+        // User unmuted
+        plugins.forEach(async function (plugin) {
+            if (typeof plugin.onUserUnmute === "function") {
+                await plugin.onUserUnmute(newState.channel);
+            }
+        });
+    } else if(!oldState.mute && newState.mute) {
+        // User muted
+        plugins.forEach(async function (plugin) {
+            if (typeof plugin.onUserMute === "function") {
+                await plugin.onUserMmute(newState.channel);
+            }
+        });
+    }
+});
+
+// Bot internal actions only
+bot.on('ready', () => {
+    log.info({ bot }, `Logged in to Discord as ${bot.user.tag}`);
 });
 
 bot.on('rateLimit', rateLimitInfo => {
